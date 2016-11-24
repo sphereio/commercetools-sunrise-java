ec89b03aea20275fab00d045e7e4863ccc166ed4
Laura Luiz <laura.luiz@commercetools.de>
2016-11-24 13:45:23 +0100

    Improve delete with retry function

--- common/it/com/commercetools/sunrise/common/TestFixtures.java
+++ common/it/com/commercetools/sunrise/common/TestFixtures.java
@@ -5,7 +5,6 @@ import io.sphere.sdk.carts.CartDraft;
 import io.sphere.sdk.carts.CartDraftDsl;
 import io.sphere.sdk.carts.commands.CartCreateCommand;
 import io.sphere.sdk.carts.commands.CartDeleteCommand;
-import io.sphere.sdk.carts.queries.CartByIdGet;
 import io.sphere.sdk.client.BlockingSphereClient;
 import io.sphere.sdk.client.ConcurrentModificationException;
 import io.sphere.sdk.client.NotFoundException;
@@ -14,29 +13,29 @@ import io.sphere.sdk.customers.*;
 import io.sphere.sdk.customers.commands.CustomerCreateCommand;
 import io.sphere.sdk.customers.commands.CustomerDeleteCommand;
 import io.sphere.sdk.customers.commands.CustomerSignInCommand;
-import io.sphere.sdk.customers.queries.CustomerByIdGet;
 import io.sphere.sdk.models.DefaultCurrencyUnits;
+import io.sphere.sdk.models.Versioned;
 
 import javax.annotation.Nullable;
 import java.util.function.Function;
-import java.util.function.Supplier;
 
 import static io.sphere.sdk.test.SphereTestUtils.randomEmail;
 
 public class TestFixtures {
 
     public static String CUSTOMER_PASSWORD = "PSW-12345";
+    private static final int DEFAULT_DELETE_TTL = 5;
 
     public static void withCart(final BlockingSphereClient client, final CartDraft cartDraft, final Function<Cart, Cart> test) {
         final Cart cart = client.executeBlocking(CartCreateCommand.of(cartDraft));
         final Cart cartAfterTest = test.apply(cart);
-        deleteCartWithRetry(client, cart.getId(), cartAfterTest);
+        deleteCartWithRetry(client, cartAfterTest);
     }
 
     public static void withCustomer(final BlockingSphereClient client, final CustomerDraft customerDraft, final Function<CustomerSignInResult, Customer> test) {
         final CustomerSignInResult customerSignInResult = client.executeBlocking(CustomerCreateCommand.of(customerDraft));
         final Customer customerAfterTest = test.apply(customerSignInResult);
-        deleteCustomerWithRetry(client, customerSignInResult.getCustomer().getId(), customerAfterTest);
+        deleteCustomerWithRetry(client, customerAfterTest);
     }
 
     public static CustomerSignInResult logInCustomer(final BlockingSphereClient client, final Customer customer, @Nullable final Cart cart) {
@@ -52,26 +51,29 @@ public class TestFixtures {
         return CustomerDraftBuilder.of(randomEmail(TestFixtures.class), TestFixtures.CUSTOMER_PASSWORD).build();
     }
 
-    private static void deleteCartWithRetry(final BlockingSphereClient client, final String cartId, final Cart cartAfterTest) {
-        deleteWithRetry(client, cartAfterTest, () -> CartByIdGet.of(cartId), CartDeleteCommand::of);
+    private static void deleteCartWithRetry(final BlockingSphereClient client, final Cart cartAfterTest) {
+        deleteWithRetry(client, cartAfterTest, CartDeleteCommand::of, DEFAULT_DELETE_TTL);
     }
 
-    private static void deleteCustomerWithRetry(final BlockingSphereClient client, final String customerId, final Customer customerAfterTest) {
-        deleteWithRetry(client, customerAfterTest, () -> CustomerByIdGet.of(customerId), CustomerDeleteCommand::of);
+    private static void deleteCustomerWithRetry(final BlockingSphereClient client, final Customer customerAfterTest) {
+        deleteWithRetry(client, customerAfterTest, CustomerDeleteCommand::of, DEFAULT_DELETE_TTL);
     }
 
-    private static <T> void deleteWithRetry(final BlockingSphereClient client, final T resource,
-                                            final Supplier<SphereRequest<T>> fetchSupplier,
-                                            final Function<T, SphereRequest<T>> deleteFunction) {
-        try {
-            client.executeBlocking(deleteFunction.apply(resource));
-        } catch (ConcurrentModificationException e) {
-            final T resourceWithCorrectVersion = client.executeBlocking(fetchSupplier.get());
-            if (resourceWithCorrectVersion != null) {
-                client.executeBlocking(deleteFunction.apply(resourceWithCorrectVersion));
+    private static <T> void deleteWithRetry(final BlockingSphereClient client, final Versioned<T> resource,
+                                            final Function<Versioned<T>, SphereRequest<T>> deleteFunction, final int ttl) {
+        if (ttl > 0) {
+            try {
+                client.executeBlocking(deleteFunction.apply(resource));
+            } catch (final ConcurrentModificationException e) {
+                if (e.getCurrentVersion() != null) {
+                    final Versioned<T> resourceWithCurrentVersion = Versioned.of(resource.getId(), e.getCurrentVersion());
+                    deleteWithRetry(client, resourceWithCurrentVersion, deleteFunction, ttl - 1);
+                }
+            } catch (final NotFoundException e) {
+                // mission indirectly accomplished
             }
-        } catch (NotFoundException e) {
-            // mission indirectly accomplished
+        } else {
+            throw new RuntimeException("Could not delete resource due to too many concurrent updates, resource: " + resource);
         }
     }
 }
