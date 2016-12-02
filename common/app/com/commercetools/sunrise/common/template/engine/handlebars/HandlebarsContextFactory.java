eecc556f164453fa9139745d1ffbd853e9e275d8
Laura Luiz <laura.luiz@commercetools.de>
2016-12-02 16:23:11 +0100

    Pass to context factory all available information

--- common/app/com/commercetools/sunrise/common/template/engine/handlebars/HandlebarsContextFactory.java
+++ common/app/com/commercetools/sunrise/common/template/engine/handlebars/HandlebarsContextFactory.java
@@ -3,6 +3,7 @@ package com.commercetools.sunrise.common.template.engine.handlebars;
 import com.commercetools.sunrise.common.template.engine.TemplateContext;
 import com.commercetools.sunrise.common.utils.ErrorFormatter;
 import com.github.jknack.handlebars.Context;
+import com.github.jknack.handlebars.Handlebars;
 import com.github.jknack.handlebars.ValueResolver;
 import com.github.jknack.handlebars.context.JavaBeanValueResolver;
 import com.github.jknack.handlebars.context.MapValueResolver;
@@ -22,26 +23,26 @@ public class HandlebarsContextFactory extends Base {
     @Inject
     private ErrorFormatter errorFormatter;
 
-    public Context create(final TemplateContext templateContext) {
+    public Context create(final Handlebars handlebars, final String templateName, final TemplateContext templateContext) {
         final List<ValueResolver> valueResolvers = valueResolvers(templateContext);
         final Context context = Context.newBuilder(templateContext.pageData())
                 .resolver(valueResolvers.toArray(new ValueResolver[valueResolvers.size()]))
                 .build();
-        fillContext(templateContext, context);
+        fillContext(context, templateContext);
         return context;
     }
 
-    protected void fillContext(final TemplateContext templateContext, final Context context) {
-        fillLocale(templateContext, context);
-        fillCmsPage(templateContext, context);
+    protected void fillContext(final Context context, final TemplateContext templateContext) {
+        fillLocale(context, templateContext);
+        fillCmsPage(context, templateContext);
     }
 
-    protected void fillCmsPage(final TemplateContext templateContext, final Context context) {
+    protected void fillCmsPage(final Context context, final TemplateContext templateContext) {
         templateContext.cmsPage()
                 .ifPresent(cmsPage -> context.data(CMS_PAGE_IN_CONTEXT_KEY, cmsPage));
     }
 
-    protected void fillLocale(final TemplateContext templateContext, final Context context) {
+    protected void fillLocale(final Context context, final TemplateContext templateContext) {
         context.data(LANGUAGE_TAGS_IN_CONTEXT_KEY, templateContext.locales().stream()
                 .map(Locale::toLanguageTag)
                 .collect(toList()));
