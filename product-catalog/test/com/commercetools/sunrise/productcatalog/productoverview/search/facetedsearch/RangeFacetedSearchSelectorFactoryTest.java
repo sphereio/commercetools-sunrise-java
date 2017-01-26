e79012883732182b133d688a24238319a1166805
jarek170989 <jaroslaw.stepien@combc.pl>
2017-01-26 11:30:27 +0100

    Add tests for RangeFacetedSearchSelectorFactory

---
+++ product-catalog/test/com/commercetools/sunrise/productcatalog/productoverview/search/facetedsearch/RangeFacetedSearchSelectorFactoryTest.java
@@ -0,0 +1,7 @@
+package com.commercetools.sunrise.productcatalog.productoverview.search.facetedsearch;
+
+/**
+ * Created by jarekstepien on 02.01.17.
+ */
+public class RangeFacetedSearchSelectorFactoryTest {
+}
