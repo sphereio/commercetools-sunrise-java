8e236b283d8e1524bc12b56a231d9697fb1ae489
mkoester <matthias.koester@commercetools.de>
2017-02-21 12:35:16 +0100

    Improved naming in test.

--- common/test/com/commercetools/sunrise/common/utils/UrlUtilsTest.java
+++ common/test/com/commercetools/sunrise/common/utils/UrlUtilsTest.java
@@ -3,7 +3,6 @@ package com.commercetools.sunrise.common.utils;
 import org.junit.Test;
 
 import java.net.URLDecoder;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,7 +11,6 @@ import static java.util.Arrays.asList;
 import static java.util.Collections.emptyList;
 import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThat;
-import static com.commercetools.sunrise.common.utils.UrlUtils.*;
 
 /**
  * Unit tests for {@link UrlUtils}.
@@ -20,7 +18,7 @@ import static com.commercetools.sunrise.common.utils.UrlUtils.*;
 public class UrlUtilsTest {
 
     @Test
-    public void buildsUrl() throws Exception {
+    public void buildUrl() throws Exception {
         final Map<String, List<String>> queryString = new HashMap<>();
 
         queryString.put("foo", asList("jacket", "shirt", "shoes"));
@@ -28,7 +26,7 @@ public class UrlUtilsTest {
         queryString.put("qux", singletonList("x"));
         queryString.put("baz", emptyList());
 
-        final String url = buildUrl("some-path", queryString);
+        final String url = UrlUtils.buildUrl("some-path", queryString);
         final String expectedUrl = "some-path?bar=1&bar=2&qux=x&foo=jacket&foo=shirt&foo=shoes";
 
         assertThat(url).isEqualTo(expectedUrl);
@@ -40,13 +38,13 @@ public class UrlUtilsTest {
     }
 
     @Test
-    public void buildsUrl_Encoded() throws Exception {
+    public void buildUrl_QueryParamValuesEncoded() throws Exception {
         final Map<String, List<String>> queryString = new HashMap<>();
 
         queryString.put("foo", asList("jacket shirt", "shoes "));
         queryString.put("bar", asList("1 2"));
 
-        final String url = buildUrl("some-path", queryString);
+        final String url = UrlUtils.buildUrl("some-path", queryString);
         assertThat(url).isEqualTo("some-path?bar=1+2&foo=jacket+shirt&foo=shoes+");
 
         final String decodedUrl = URLDecoder.decode(url, "UTF-8");
