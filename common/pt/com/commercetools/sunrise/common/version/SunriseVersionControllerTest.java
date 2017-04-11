49c194e1a48b2fd8d6f0a235e9cc73c37dd4f639
Laura Luiz <laura.luiz@commercetools.de>
2017-04-11 10:03:57 +0200

    Remove output logger

--- common/pt/com/commercetools/sunrise/common/version/SunriseVersionControllerTest.java
+++ common/pt/com/commercetools/sunrise/common/version/SunriseVersionControllerTest.java
@@ -1,19 +1,13 @@
 package com.commercetools.sunrise.common.version;
 
-import ch.qos.logback.classic.LoggerContext;
-import ch.qos.logback.core.joran.util.ConfigurationWatchListUtil;
 import org.junit.Test;
-import org.slf4j.Logger;
 import play.Application;
 import play.mvc.Http;
 import play.mvc.Result;
 import play.test.WithApplication;
 
-import java.net.URL;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static play.test.Helpers.contentAsString;
-import static play.test.Helpers.fakeApplication;
 
 public class SunriseVersionControllerTest extends WithApplication {
 
@@ -23,12 +17,6 @@ public class SunriseVersionControllerTest extends WithApplication {
         assertThat(result.status()).isEqualTo(Http.Status.OK);
         assertThat(result.contentType()).contains(Http.MimeTypes.JSON);
         assertThat(contentAsString(result)).contains("version").contains("build");
-
-        final Logger logger = org.slf4j.LoggerFactory.getLogger("play");
-        LoggerContext loggerContext = ((ch.qos.logback.classic.Logger)logger).getLoggerContext();
-        URL mainURL = ConfigurationWatchListUtil.getMainWatchURL(loggerContext);
-        System.out.println(mainURL);
-        logger.debug("PRinting stufff test");
     }
 
     private static class VersionControllerTest extends SunriseVersionController {
