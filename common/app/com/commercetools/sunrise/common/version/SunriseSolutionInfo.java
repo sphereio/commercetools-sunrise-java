91cbd461afab9a21aabe1b86c85de717b9d1a56d
abeniasaad <achraf.beniasaad@commercetools.de>
2017-06-08 09:39:57 +0200

    making the version dynamic and the bean constructor public

--- common/app/com/commercetools/sunrise/common/version/SunriseSolutionInfo.java
+++ common/app/com/commercetools/sunrise/common/version/SunriseSolutionInfo.java
@@ -1,13 +1,42 @@
 package com.commercetools.sunrise.common.version;
 
 import io.sphere.sdk.client.SolutionInfo;
+import io.sphere.sdk.json.SphereJsonUtils;
+import org.apache.commons.io.IOUtils;
+import play.api.Application;
+import play.api.Play;
+
+import javax.inject.Inject;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 public class SunriseSolutionInfo extends SolutionInfo {
 
-    protected SunriseSolutionInfo() {
+
+    public SunriseSolutionInfo() {
 
         setName("sunrise-java-shop-framework");
-        setVersion("1.0.0-M4-SNAPSHOT");
+        setVersion(getVersionFromJson());
+        setEmergencyContact("");
+        setWebsite("");
+
 
     }
+
+    private static String getVersionFromJson(){
+
+        String version ;
+        try {
+            final InputStream versionAsStream = Play.current().resourceAsStream("internal/version.json").get();
+            final String versionAsString = IOUtils.toString(versionAsStream, StandardCharsets.UTF_8);
+            version = SphereJsonUtils.parse(versionAsString).get("version").asText();
+        }
+        catch (IOException e){
+            version = "UNKNOWN";
+        }
+        return version;
+    }
+
+
 }
