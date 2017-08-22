30abd48aaed43bb2508058828c1caeeed1279a7a
mkoester <matthias.koester@commercetools.de>
2017-08-22 13:42:08 +0200

    First version with configuration class.

--- app/EmailSenderProvider.java
+++ app/EmailSenderProvider.java
@@ -1,33 +1,23 @@
 import com.google.inject.Provider;
 import io.commercetools.sunrise.email.EmailSender;
 import io.commercetools.sunrise.email.smtp.SmtpAuthEmailSender;
-import io.commercetools.sunrise.email.smtp.SmtpConfiguration;
-import play.Configuration;
 
 import javax.inject.Inject;
+import java.util.concurrent.Executor;
 import java.util.concurrent.ForkJoinPool;
 
 
 public final class EmailSenderProvider implements Provider<EmailSender> {
-    private final SmtpConfiguration smtpConfiguration;
-    private final ForkJoinPool executor;
-    private final int timeoutMs;
+    private final EmailSenderSettings emailSenderSettings;
 
     @Inject
-    protected EmailSenderProvider(final Configuration configuration) {
-        final String host = configuration.getString("application.smtp.host");
-        final int port = configuration.getInt("application.smtp.port", 25);
-        final String username = configuration.getString("application.smtp.username");
-        final String password = configuration.getString("application.smtp.password");
-        final String security = configuration.getString("application.smtp.security");
-        timeoutMs = configuration.getInt("application.smtp.timeoutMs", 10 * 1000);
-        smtpConfiguration =
-                new SmtpConfiguration(host, port, SmtpConfiguration.TransportSecurity.valueOf(security), username, password);
-        executor = new ForkJoinPool(5);
+    protected EmailSenderProvider(final EmailSenderSettings emailSenderSettings) {
+        this.emailSenderSettings = emailSenderSettings;
     }
 
     @Override
     public EmailSender get() {
-        return new SmtpAuthEmailSender(smtpConfiguration, executor, timeoutMs);
+        final Executor executor = ForkJoinPool.commonPool();
+        return new SmtpAuthEmailSender(emailSenderSettings.smtpConfiguration(), executor, emailSenderSettings.timeoutMs());
     }
 }
