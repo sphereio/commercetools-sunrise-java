0d97579b1e95f59d3684d05e4566906da0332a7c
Laura Luiz <laura.luiz@commercetools.de>
2017-09-18 17:46:13 +0200

    Adapt scopes implementation on new structure

--- common/app/com/commercetools/sunrise/ctp/client/SphereClientConfigProvider.java
+++ common/app/com/commercetools/sunrise/ctp/client/SphereClientConfigProvider.java
@@ -3,17 +3,19 @@ package com.commercetools.sunrise.ctp.client;
 import com.commercetools.sunrise.play.configuration.SunriseConfigurationException;
 import com.google.inject.Provider;
 import io.sphere.sdk.client.SphereClientConfig;
+import io.sphere.sdk.client.SphereClientConfigBuilder;
 import play.Configuration;
 
 import javax.inject.Inject;
 
 public final class SphereClientConfigProvider implements Provider<SphereClientConfig> {
 
+    private static final String CONFIG_ROOT = "sunrise.ctp.client";
     private final Configuration configuration;
 
     @Inject
     SphereClientConfigProvider(final Configuration globalConfig) {
-        this.configuration = globalConfig.getConfig("sunrise.ctp.client");
+        this.configuration = globalConfig.getConfig(CONFIG_ROOT);
     }
 
     @Override
@@ -22,11 +24,13 @@ public final class SphereClientConfigProvider implements Provider<SphereClientCo
             final String projectKey = configuration.getString("projectKey");
             final String clientId = configuration.getString("clientId");
             final String clientSecret = configuration.getString("clientSecret");
-            return SphereClientConfig.of(projectKey, clientId, clientSecret)
-                    .withApiUrl(configuration.getString("apiUrl"))
-                    .withAuthUrl(configuration.getString("authUrl"));
+            return SphereClientConfigBuilder.ofKeyIdSecret(projectKey, clientId, clientSecret)
+                    .authUrl(configuration.getString("authUrl"))
+                    .apiUrl(configuration.getString("apiUrl"))
+                    .scopeStrings(configuration.getStringList("scopes"))
+                    .build();
         } catch (IllegalArgumentException e) {
-            throw new SunriseConfigurationException("Could not initialize SphereClientConfig", "ctp.client", e);
+            throw new SunriseConfigurationException("Could not initialize SphereClientConfig", CONFIG_ROOT, e);
         }
     }
 }
