e719e09aa38f852e1d9266ac313b82263051ae7d
Laura Luiz <laura.luiz@commercetools.de>
2017-09-22 14:23:36 +0200

    Move provider logic to provider class

--- common/app/com/commercetools/sunrise/ctp/project/ProjectModule.java
+++ common/app/com/commercetools/sunrise/ctp/project/ProjectModule.java
@@ -1,25 +1,14 @@
 package com.commercetools.sunrise.ctp.project;
 
 import com.google.inject.AbstractModule;
-import com.google.inject.Provides;
-import io.sphere.sdk.client.SphereClient;
-import io.sphere.sdk.client.SphereRequest;
 import io.sphere.sdk.projects.Project;
-import io.sphere.sdk.projects.queries.ProjectGet;
 
-import java.time.Duration;
-
-import static io.sphere.sdk.client.SphereClientUtils.blockingWait;
+import javax.inject.Singleton;
 
 public final class ProjectModule extends AbstractModule {
 
     @Override
     protected void configure() {
-    }
-
-    @Provides
-    public Project provideProject(final SphereClient sphereClient) {
-        final SphereRequest<Project> request = ProjectGet.of();
-        return blockingWait(sphereClient.execute(request), Duration.ofSeconds(30));
+        bind(Project.class).toProvider(ProjectProvider.class).in(Singleton.class);
     }
 }
---
+++ common/app/com/commercetools/sunrise/ctp/project/ProjectProvider.java
@@ -0,0 +1,28 @@
+package com.commercetools.sunrise.ctp.project;
+
+import io.sphere.sdk.client.SphereClient;
+import io.sphere.sdk.client.SphereRequest;
+import io.sphere.sdk.projects.Project;
+import io.sphere.sdk.projects.queries.ProjectGet;
+
+import javax.inject.Inject;
+import javax.inject.Provider;
+import java.time.Duration;
+
+import static io.sphere.sdk.client.SphereClientUtils.blockingWait;
+
+public final class ProjectProvider implements Provider<Project> {
+
+    private final SphereClient sphereClient;
+
+    @Inject
+    ProjectProvider(final SphereClient sphereClient) {
+        this.sphereClient = sphereClient;
+    }
+
+    @Override
+    public Project get() {
+        final SphereRequest<Project> request = ProjectGet.of();
+        return blockingWait(sphereClient.execute(request), Duration.ofSeconds(30));
+    }
+}
