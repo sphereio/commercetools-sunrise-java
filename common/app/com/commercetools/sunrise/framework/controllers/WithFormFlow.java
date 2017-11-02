592692f3c719afb274f0d9a35994a01356cd94c5
Laura Luiz <laura.luiz@commercetools.de>
2017-11-02 10:28:59 +0100

    Add Javadoc for saveMessage

--- common/app/com/commercetools/sunrise/framework/controllers/WithFormFlow.java
+++ common/app/com/commercetools/sunrise/framework/controllers/WithFormFlow.java
@@ -5,6 +5,7 @@ import io.sphere.sdk.client.ClientErrorException;
 import org.slf4j.Logger;
 import play.data.Form;
 import play.libs.concurrent.HttpExecution;
+import play.mvc.Http;
 import play.mvc.Result;
 
 import java.util.concurrent.CompletionStage;
@@ -73,7 +74,25 @@ public interface WithFormFlow<I, O, F> extends WithForm<F> {
         getLogger().error("The CTP request raised an unexpected exception", throwable);
     }
 
-    default void saveMessage(final MessageType type, final String message) {
-        flash().put(type.name(), message);
+    /**
+     * Saves a message of a certain {@link MessageType} meant to be displayed to the user after a redirection to
+     * another page.
+     *
+     * Example use case: the user submitted a successful form, then the user is redirected to a page where the message
+     * informing of the successful action is shown.
+     * In this case {@code saveMessage(MessageType.SUCCESS, "The action was successful!")} is invoked right before
+     * the redirection is executed.
+     *
+     * @param messageType type of the message to be saved (e.g. success, info)
+     * @param message the content of the message to be saved
+     */
+    default void saveMessage(final MessageType messageType, final String message) {
+        final String key = messageType.name();
+        final Http.Flash flash = flash();
+        if (flash.containsKey(key)) {
+            getLogger().warn("Replacing a message of type \"{}\" within the same request\nOld message: \"{}\"\nNew message: \"{}\"",
+                    key, flash.get(key), message);
+        }
+        flash.put(key, message);
     }
 }
