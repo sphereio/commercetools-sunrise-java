dc2599b116826da95f5509ce89fab81a229884bc
Laura Luiz <laura.luiz@commercetools.de>
2017-11-14 16:34:51 +0100

    Reorganize methods

--- common/app/play/ext/i18n/MessagesModule.scala
+++ common/app/play/ext/i18n/MessagesModule.scala
@@ -17,8 +17,14 @@ class YamlMessagesApi @Inject()(environment: Environment, configuration: Configu
 
 
   override protected def loadMessages(file: String): Map[String, String] = {
+
     def loadMessages(file: String, loader: MessagesLoader): Map[String, String] = {
       import scala.collection.JavaConverters._
+
+      def joinPaths(first: Option[String], second: String) = first match {
+        case Some(parent) => new java.io.File(parent, second).getPath
+        case None => second
+      }
 
       environment.classLoader.getResources(joinPaths(messagesPrefix, file)).asScala.toList
         .filterNot(url => Resources.isDirectory(environment.classLoader, url)).reverse
@@ -31,39 +37,35 @@ class YamlMessagesApi @Inject()(environment: Environment, configuration: Configu
   }
 
   override def translate(key: String, args: Seq[Any])(implicit lang: Lang): Option[String] = {
+
+    def filterHashArgsEntries(args: Seq[Any]): Seq[Entry[String, Any]] = {
+      def isHashArgsEntry(entry: Any) = entry.isInstanceOf[Entry[Any, Any]] && entry.asInstanceOf[Entry[Any, Any]].getKey.isInstanceOf[String]
+      args.filter(isHashArgsEntry)
+        .map(_.asInstanceOf[Entry[String, Any]])
+    }
+
+    def findAppliedKey(key: String, args: Seq[Entry[String, Any]]): String = {
+      def isPluralMessage(args: Seq[Entry[String, Any]]) = {
+        args.filter(_.getKey == "count")
+          .map(_.getValue)
+          .filter(_.isInstanceOf[Number])
+          .exists(_.asInstanceOf[Number].doubleValue != 1)
+      }
+      def pluralizeKey(key: String): String = key + "_plural"
+      if (isPluralMessage(args)) pluralizeKey(key) else key
+    }
+
+    def replaceParameters(pattern: String, args: Seq[Entry[String, Any]]): String = {
+      args.filter(_.getValue != null)
+        .foldLeft(pattern)((acc, entry) => acc.replace("__" + entry.getKey + "__", entry.getValue.toString))
+    }
+
     val codesToTry = Seq(lang.code, lang.language, "default", "default.play")
     val hashArgs = filterHashArgsEntries(args)
     val appliedKey = findAppliedKey(key, hashArgs)
     val pattern: Option[String] = codesToTry.foldLeft[Option[String]](None)((res, code) =>
       res.orElse(messages.get(code).flatMap(_.get(appliedKey))))
     pattern.map(pattern => replaceParameters(pattern, hashArgs))
-  }
-
-  private def joinPaths(first: Option[String], second: String) = first match {
-    case Some(parent) => new java.io.File(parent, second).getPath
-    case None => second
-  }
-
-  private def filterHashArgsEntries(args: Seq[Any]): Seq[Entry[String, Any]] = {
-    def isHashArgsEntry(entry: Any) = entry.isInstanceOf[Entry[Any, Any]] && entry.asInstanceOf[Entry[Any, Any]].getKey.isInstanceOf[String]
-    args.filter(isHashArgsEntry)
-      .map(_.asInstanceOf[Entry[String, Any]])
-  }
-
-  private def findAppliedKey(key: String, args: Seq[Entry[String, Any]]): String = {
-    def isPluralMessage(args: Seq[Entry[String, Any]]) = {
-      args.filter(_.getKey == "count")
-        .map(_.getValue)
-        .filter(_.isInstanceOf[Number])
-        .exists(_.asInstanceOf[Number].doubleValue != 1)
-    }
-    def pluralizeKey(key: String): String = key + "_plural"
-    if (isPluralMessage(args)) pluralizeKey(key) else key
-  }
-
-  private def replaceParameters(pattern: String, args: Seq[Entry[String, Any]]): String = {
-    args.filter(_.getValue != null)
-      .foldLeft(pattern)((acc, entry) => acc.replace("__" + entry.getKey + "__", entry.getValue.toString))
   }
 
   // CMS messages API only: loadAllMessages, adding "filenames"
