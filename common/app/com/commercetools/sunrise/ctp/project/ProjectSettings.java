e18236b4b2e6c4fa36a9734c5e091be7374f2c6c
Laura Luiz <laura.luiz@commercetools.de>
2017-11-30 16:19:49 +0100

    Remove unncessary fields for project settings and update config

--- common/app/com/commercetools/sunrise/ctp/project/ProjectSettings.java
+++ common/app/com/commercetools/sunrise/ctp/project/ProjectSettings.java
@@ -1,12 +1,8 @@
 package com.commercetools.sunrise.ctp.project;
 
 import com.google.inject.ImplementedBy;
-import com.neovisionaries.i18n.CountryCode;
 import play.Configuration;
 
-import javax.money.CurrencyUnit;
-import java.util.List;
-import java.util.Locale;
 import java.util.Optional;
 
 @ImplementedBy(ProjectSettingsImpl.class)
@@ -21,21 +17,6 @@ public interface ProjectSettings {
      * @return time in seconds until the project in cache expires
      */
     Optional<Integer> cacheExpiration();
-
-    /**
-     * @return list of languages supported by the project
-     */
-    List<Locale> languages();
-
-    /**
-     * @return list of countries supported by the project
-     */
-    List<CountryCode> countries();
-
-    /**
-     * @return list of currencies supported by the project
-     */
-    List<CurrencyUnit> currencies();
 
     static ProjectSettings of(final Configuration globalConfig, final String configPath) {
         return new ProjectSettingsImpl(globalConfig, configPath);
--- common/app/com/commercetools/sunrise/ctp/project/ProjectSettingsImpl.java
+++ common/app/com/commercetools/sunrise/ctp/project/ProjectSettingsImpl.java
@@ -1,27 +1,16 @@
 package com.commercetools.sunrise.ctp.project;
 
-import com.neovisionaries.i18n.CountryCode;
 import play.Configuration;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
-import javax.money.CurrencyUnit;
-import javax.money.Monetary;
-import java.util.List;
-import java.util.Locale;
 import java.util.Optional;
-
-import static java.util.Collections.emptyList;
-import static java.util.stream.Collectors.toList;
 
 @Singleton
 class ProjectSettingsImpl implements ProjectSettings {
 
     private final String cacheKey;
     private final Integer cacheExpiration;
-    private final List<Locale> languages;
-    private final List<CountryCode> countries;
-    private final List<CurrencyUnit> currencies;
 
     @Inject
     ProjectSettingsImpl(final Configuration configuration) {
@@ -32,15 +21,6 @@ class ProjectSettingsImpl implements ProjectSettings {
         final Configuration config = globalConfig.getConfig(configPath);
         this.cacheKey = config.getString("cacheKey");
         this.cacheExpiration = config.getInt("cacheExpiration");
-        this.languages = config.getStringList("languages", emptyList()).stream()
-                .map(Locale::forLanguageTag)
-                .collect(toList());
-        this.countries = config.getStringList("countries", emptyList()).stream()
-                .map(CountryCode::getByCode)
-                .collect(toList());
-        this.currencies = config.getStringList("currencies", emptyList()).stream()
-                .map(Monetary::getCurrency)
-                .collect(toList());
     }
 
     @Override
@@ -51,20 +31,5 @@ class ProjectSettingsImpl implements ProjectSettings {
     @Override
     public Optional<Integer> cacheExpiration() {
         return Optional.ofNullable(cacheExpiration);
-    }
-
-    @Override
-    public List<Locale> languages() {
-        return languages;
-    }
-
-    @Override
-    public List<CountryCode> countries() {
-        return countries;
-    }
-
-    @Override
-    public List<CurrencyUnit> currencies() {
-        return currencies;
     }
 }
